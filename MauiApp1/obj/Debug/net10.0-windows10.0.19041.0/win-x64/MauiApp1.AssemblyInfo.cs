360d609721f0e3c5fc6acbb7b4e75067e0c5e427
Junkai Wang <Junkai.Wang@uon.edu.au>
2026-03-15 18:13:09 +0800

    The third submission of the patch fixed the spelling errors of the words.

--- MauiApp1/obj/Debug/net10.0-windows10.0.19041.0/win-x64/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/obj/Debug/net10.0-windows10.0.19041.0/win-x64/MauiApp1.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0fde9498a7127c067236615aa1e2661b6d50c968")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+415f06d8e78890af1ef9424a54fae301b5feec01")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
